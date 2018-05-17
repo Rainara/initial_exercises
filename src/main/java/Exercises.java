2e00ff3167bcfd71d9276f01c6d636a4c237c3a9
Rainara <rainaramateus@gmail.com>
2018-05-17 07:23:19 -0300

    Diamond and FizzBuzz

--- src/main/java/Exercises.java
+++ src/main/java/Exercises.java
@@ -4,19 +4,66 @@ public class Exercises {
 
         StringBuilder output = new StringBuilder();
 
-        for (int i=1 ;i<=number ; i++) {
-            for (int j=1 ;j<=i ;j++)
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++)
                 output.append("*");
             output.append("\n");
         }
         return output.toString();
 
 
-
     }
 
-    public String diamondTriangle(int i) {
 
-        return "";
+    public String diamondTriangle(int number) {
+
+
+        StringBuilder output = new StringBuilder();
+        for (int i = 1; i <= number - 1; i++) {
+            for (int j = 1; j <= number - i; j++)
+                output.append(" ");
+            for (int j = 1; j <= i * 2 - 1; j++)
+                output.append("*");
+            output.append("\n");
+        }
+
+        output.append("Rainara\n");
+
+        for (int i = number - 1; i > 0; i--) {
+            for (int j = 1; j <= number - i; j++)
+                output.append(" ");
+            for (int j = 1; j <= i * 2 - 1; j++)
+                output.append("*");
+            output.append("\n");
+        }
+
+        return output.toString();
+    }
+
+    public String fizzBuzzExercise(int number) {
+
+
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 1; i <= number; i++) {
+
+            if (i % 15 == 0) {
+                output.append("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+
+                output.append("Fizz");
+            } else if (i % 5 == 0) {
+                output.append("Buzz");
+            } else {
+                output.append(i);
+            }
+            output.append("\n");
+
+        }
+
+        return output.toString();
+
+
     }
 }
